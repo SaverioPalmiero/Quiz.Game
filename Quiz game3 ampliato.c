622c2988ecc0bfb51e1a286173e86212fa016d28
SAVERIO <109146940+SaverioPalmiero@users.noreply.github.com>
2022-07-14 16:59:30 +0200

    Add files via upload

---
+++ Quiz game3 ampliato.c
@@ -0,0 +1,91 @@
+//Inizio scegliendo le librerie
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Inizio a scrivere il programma
+main ()
+{
+	//Tramite il comando printf vado a descrivere ciò che sara' il gioco
+	printf("\n\n                                ################### QUIZ GAME ################## \n\n");
+	printf("                                   Questo sara' un quiz a risposta multipla \n");
+	printf("                                  Ad ogni risposta esatta riceverai dieci punti \n");
+	printf("                                  Ad ogni risposta errata perderai dici punti \n\n\n");
+	
+	//Definisco una variabile 
+	char z;
+	
+	
+	
+	//Indico le varie alternative che avra' il giocatore 
+	
+	printf("\nBENE!");
+	printf("\nPremere il tasto z per partire con la prima domanda ");
+	printf("\nPremere il tasto x per uscire dal gioco ");
+	scanf("%c",&z);
+	
+	
+	
+		//Definisco le variabili
+		
+char a ;
+char b ;
+char c ;
+scanf("%c",&a,&b,&c);
+
+ /*a=roma;
+ b=azzurro;
+ c=quattro;*/
+
+//Scrivo le Domande
+
+
+    printf("\n\n\n Quale e'la capitale d'Italia ? ");
+    printf("\n\n1) Napoli\n");
+    printf("\n2) Roma\n");
+    printf("\n3) Torino\n");
+    printf("\n\nInserisci il numero della risposta esatta\n");
+    scanf("%c",&a );
+    
+    
+    printf("\n\n\n Di che colore e' il cielo ? ");
+    printf("\n\n1) Azzurro\n");
+    printf("\n2) Nero\n");
+    printf("\n3) Giallo\n");
+    printf("\n\nInserisci il numero della risposta esatta\n");
+    scanf("%c",&b);
+    
+    printf("\n\n\n Quante zampe ha un cane ? ");
+    printf("\n\n1) Dodici\n");
+    printf("\n2) Venti\n");
+    printf("\n3) Quattro\n");
+    printf("\n\nInserisci il numero della risposta esatta\n");
+    scanf("%c",&c);
+    
+    
+    //Indico la fine del quiz 
+    
+    printf("\n\n\n*************QUIZ FINITO*************");
+    
+    //Indico le varie sclete al giocatore
+    
+    printf("\n\n\n\nSe vuoi ricominciare premi r");
+    printf("\n\nSe vuoi uscire premi x");
+    
+    
+    //Indico il punteggio ottenuto dal giocatore 
+    
+    printf("\n\n\n                                            Il tuo punteggio e'di .... ");
+    
+    getchar();
+	
+		}
+	
+	
+
+	
+
+		
+
+
+
